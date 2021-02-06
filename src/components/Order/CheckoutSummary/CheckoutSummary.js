255c0aeaa2d39e4d3f92ae59f8a1193184e456ca
Adnan Ahmed <adnan_a96@hotmail.com>
2021-02-06 01:18:43 -0500

    added advanced redux features

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -7,7 +7,7 @@ const checkoutSummary = (props) => {
     return (
         <div className={classes.CheckoutSummary}>
             <h1>We hope it tastes good!</h1>
-            <div style={{width: '100%', margin: 'auto'}}>
+            <div style={{width: '80%', margin: 'auto'}}>
                 <Burger ingredients={props.ingredients}/>
             </div>
             <Button 
